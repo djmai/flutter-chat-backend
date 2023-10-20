c8dc19c6c93f6656a56de944bdbcc1c76e1c39c7
Miguel Martínez <mai.djmai@gmail.com>
2023-10-20 00:10:04 -0600

    Update configuration file

--- database/config.js
+++ database/config.js
@@ -4,7 +4,13 @@ const mongoose = require('mongoose');
 
 const dbConnection = async () => {
   try {
-    const uri = `mongodb+srv://${process.env.USER_DB}:${process.env.PASS_DB}@${process.env.HOST_DB}/${process.env.DB_NAME}`;
+    var uri = `mongodb+srv://${process.env.USER_DB}:${process.env.PASS_DB}@${process.env.HOST_DB}/${process.env.DB_NAME}`;
+
+    if (process.env.ENVIRONMENT == 'development') {
+      uri = `mongodb://${process.env.HOST_DB}/${process.env.DB_NAME}?readPreference=primary&appname=MongoDB%20Compass&ssl=false`;
+    }
+
+    mongodb://localhost:27017/?readPreference=primary&appname=MongoDB%20Compass&ssl=false
     await mongoose.connect(uri, { useNewUrlParser: true, useUnifiedTopology: true })
     // .then(() => console.log(`Conectado con la base de datos "${process.env.DB_NAME}"`))
     // .catch(e => console.log('error db:', e));
